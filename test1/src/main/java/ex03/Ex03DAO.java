4273b7a8f27ff9e22f8b44fdbd5a8a962054615e
daabiiny <daabiiny@gmail.com>
2023-12-04 15:59:14 +0900

    231204

--- test1/src/main/java/ex03/Ex03DAO.java
+++ test1/src/main/java/ex03/Ex03DAO.java
@@ -1,6 +1,8 @@
 package ex03;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ex03DAO {
 	
@@ -45,6 +47,37 @@ public class Ex03DAO {
 			close();
 		}
 		return answer;
+	}
+	
+	public List<EmployeeDTO> getEmployeeList() {
+		ArrayList<EmployeeDTO> list = new ArrayList<>();
+		String sql = "select * from employees";
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				EmployeeDTO dto = new EmployeeDTO();
+				dto.setCommission_pct(rs.getDouble("commission_pct"));
+				dto.setDepartment_id(rs.getInt("department_id"));
+				dto.setEmail(rs.getString("email"));
+				dto.setEmployee_id(rs.getInt("employee_id"));
+				dto.setFirst_name(rs.getString("first_name"));
+				dto.setHire_date(rs.getDate("hire_date"));
+				dto.setJob_id(rs.getString("job_id"));
+				dto.setLast_name(rs.getString("last_name"));
+				dto.setManager_id(rs.getInt("manager_id"));
+				dto.setPhone_number(rs.getString("phone_number"));
+				dto.setSalary(rs.getDouble("salary"));
+				list.add(dto);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally { close(); }
+		
+		return list;
 	}
 	
 }
